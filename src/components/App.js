f34bb9d5d778fcb3e77b5a0a395ef8b689a906d3
Giancarlo Vera <gianverab@gmail.com>
2018-10-28 22:49:18 -0300

    adding our first component

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import StorePicker from './StorePicker';
+
+const App = () => (
+  <StorePicker />
+);
+
+export default App;
---
+++ src/components/StorePicker.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StorePicker = () => (
+  <form action="" className="store-selector">
+    <h2>Please enter a Store!</h2>
+    <input type="text" required placeholder="Store Name" />
+    <button type="submit">Visit Store -></button>
+  </form>
+);
+
+export default StorePicker;
