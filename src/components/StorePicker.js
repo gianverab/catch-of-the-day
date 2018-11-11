a8f5dcd256d768fb3a20d1db5cb1ff35efbbf095
Giancarlo Vera <gianverab@gmail.com>
2018-11-11 18:52:58 -0300

    handling routing event for storepicker

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { getFunName } from '../helpers';
 
-const StorePicker = () => (
-  <form action="" className="store-selector">
-    <h2>Please enter a Store!</h2>
-    <input type="text" required placeholder="Store Name" />
-    <button type="submit">Visit Store -&gt;</button>
-  </form>
-);
+class StorePicker extends Component {
+  storeInput = React.createRef()
+  handleSubmitStore = (event) => {
+    event.preventDefault();
+    const storeName = this.storeInput.current.value;
+    this.props.history.push(`store/${storeName}`);
+  }
+  render() {
+    return (
+      <form action="" className="store-selector" onSubmit={this.handleSubmitStore}>
+        <h2>Please enter a Store!</h2>
+        <input
+          type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+          ref={this.storeInput}
+        />
+        <button type="submit">Visit Store -&gt;</button>
+      </form>
+    );
+  }
+}
 
 export default StorePicker;
