e4cc7ac09f98d0939257416b4e3ef13bfb0f88b1
Giancarlo Vera <gianverab@gmail.com>
2019-04-02 09:29:29 -0500

    displaying order state

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { formatPrice } from '../helpers';
 
-const Order = ({ fishes, order }) => (
-  <div className="order-wrap">
-    <h2>Order!!!</h2>
-    <ul>
-      {Object.keys(order).map(key => (
-        <li key={key} index={key} details={order[key]}>
-          {}
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+class Order extends Component {
+  renderOrder = key => {
+    const fish = this.props.fishes[key];
+    const count = this.props.order[key];
+    const isAvailable = fish && fish.status === 'available';
+    if (!isAvailable) {
+      return <li key={key}>Sorry {fish ? fish.name : 'fish'} is no longer available</li>;
+    }
+    return (
+      <li key={key}>
+        {count} lbs {fish.name}
+        {formatPrice(count * fish.price)}
+      </li>
+    );
+  };
+  render() {
+    const { fishes, order } = this.props;
+    const orderIds = Object.keys(order);
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = fishes[key];
+      const count = order[key];
+      const isAvailable = fish && fish.status === 'available';
+      if (isAvailable) {
+        return prevTotal + count * fish.price;
+      }
+      return prevTotal;
+    }, 0);
+    return (
+      <div className="order-wrap">
+        <h2>Order</h2>
+        <ul className="order">{orderIds.map(this.renderOrder)}</ul>
+        <div className="total">
+          Total:
+          <strong>{formatPrice(total)}</strong>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Order;
